bd630a4c59e2ffc132273eeb45c641581f6ac4ee
Richard Kim <richeekim@gmail.com>
2018-04-07 03:55:16 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,10 +3,3 @@ class Hash
     map {|key, value| arguments.include?(value) ? key : nil }.compact
   end
 end
-
-# meals = Hash.new
-#   meals["breakfast"] = "eggs"
-#   meals["lunch"] = "fish"
-#   meals["dinner"] = "steak"
-#
-#   meals.keys_of("steak")
