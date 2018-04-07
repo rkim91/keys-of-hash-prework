13e7e76ad9e0a31d15a2cb934b49469fdd5c159a
Richard Kim <richeekim@gmail.com>
2018-04-07 03:52:29 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
-    map {|key, value| *arguments.include?(value) ? key : nil }.compact
+    map {|key, value| arguments.include?(value) ? key : nil }.compact
   end
 end
 
@@ -8,5 +8,5 @@ end
 #   meals["breakfast"] = "eggs"
 #   meals["lunch"] = "fish"
 #   meals["dinner"] = "steak"
-#
+# 
 #   meals.keys_of("steak")
